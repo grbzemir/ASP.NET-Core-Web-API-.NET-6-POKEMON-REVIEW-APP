18f142ed48bf5ba1c788b27e4e2b06dbb04c2418
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2023-12-23 21:27:04 +0300

    Add project files.

---
+++ PokemonReviewApp/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+    
+        public string Name { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public string Text { get; set; }
+    }
+}
